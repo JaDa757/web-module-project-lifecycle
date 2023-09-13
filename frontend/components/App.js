4d6e6f43142e7b5282456c96f0fd3c5dafd88a7e
Joshua Ackerman <joshackerman757@gmail.com>
2023-09-13 16:45:20 -0400

    concat network calls

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -22,11 +22,11 @@ export default class App extends React.Component {
   resetForm = () => {
     this.setState({ ...this.state, todoNameInput: '' })
   }
-  
+
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
       .then(res => {
-        this.fetchAllTodos()
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
         this.resetForm()
       })
       .catch(this.setAxiosResponseError)
