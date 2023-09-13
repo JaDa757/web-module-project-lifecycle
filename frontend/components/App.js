7a5450dbef2fabf9b3a1c01408bc2bebff27298c
Joshua Ackerman <joshackerman757@gmail.com>
2023-09-13 16:28:23 -0400

    post new todo fixed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,16 +7,21 @@ export default class App extends React.Component {
   state = {
     todos: [],
     error: '',
+    todoNameInput: '',
   }
 
+  onTodoNameInputChange = evt => {
+    const { value } = evt.target // always extract what you want before you use setState
+    this.setState({...this.state, todoNameInput: value })
+  }
   fetchAllTodos = () => {
     axios.get(URL)
-    .then(res => {
-      this.setState({ ...this.state, todos: res.data.data})
-    })
-    .catch(err => {      
-      this.setState({...this.state, error: err.response.data.message})
-    })
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(err => {
+        this.setState({ ...this.state, error: err.response.data.message })
+      })
   }
   componentDidMount() {
     this.fetchAllTodos()
@@ -27,14 +32,20 @@ export default class App extends React.Component {
         <div id='error'>{this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
-         {
-          this.state.todos.map((td) => {
-            return <div key={td.id}> {td.name}</div>
-          })
-         }
+          {
+            this.state.todos.map((td) => {
+              return <div key={td.id}> {td.name}</div>
+            })
+          }
         </div>
         <form id='todoForm'>
-          <input type='text' placeholder='Type todo'></input>
+          <input
+            value={this.state.todoNameInput}
+            onChange={this.onTodoNameInputChange}
+            type='text'
+            placeholder='Type todo'>
+          </input>
+
           <input type='submit'></input>
           <button>Clear Completed</button>
         </form>
